a031066fe8bd4453bbbaef4f96e4631df4a7b610
ZachZimmerman7 <zzimmerman@gmail.com>
2016-08-17 11:11:08 -0600

    Gun functionality done

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pistol : MonoBehaviour {
+
+	public int damage = 50;
+	public int ammo = 30;
+	//Optional
+	public float range = 50;
+
+	private Transform mainCamera;
+
+	private AudioSource myAudioSource;
+	public AudioClip shootSound;
+
+	// Use this for initialization
+	void Start () 
+
+	{
+		myAudioSource = GetComponent<AudioSource>();
+		mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1") && ammo > 0) 
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot()
+	{
+		//Raycast
+		Ray ray = new Ray(mainCamera.position, mainCamera.forward);
+		RaycastHit hit;
+
+		if(Physics.Raycast(ray, out hit, range))
+		{
+			if(hit.collider.CompareTag("Enemy"))
+			{
+				//Damage the Enemy
+				Debug.Log("We hit an Enemy. Yeah");
+			}
+		}
+		myAudioSource.PlayOneShot(shootSound);
+		ammo--;
+	}
+}
