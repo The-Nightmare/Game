539b2f270fe2c3c3c486cd319a25df59328ac9ee
ZachZimmerman7 <zzimmerman@gmail.com>
2016-08-18 11:34:36 -0600

    Added script

---
+++ Assets/Scripts/Enemy_Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Health : MonoBehaviour 
+{
+
+
+	public int maxHealth = 100;
+	private int currentHealth;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentHealth = maxHealth;
+		animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	public void TakeDamage (int _damage) 
+	{
+		currentHealth -= _damage;
+		animator.SetTrigger("isHit");
+
+		if (currentHealth <= 0) 
+		{
+			//Call Die Method
+		}
+		
+	}
+	void Die()
+	{
+		//Kill the Enemy
+		//Play an animation
+		//Instantiate a ragdoll
+
+		//Destroy the Enemy GO
+		Destroy(gameObject);
+	}	
+}
---
+++ Assets/Scripts/Player_Health.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Health : MonoBehaviour 
+{
+
+
+	public int maxHealth = 100;
+	private int currentHealth;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentHealth = maxHealth;
+
+	}
+
+	// Update is called once per frame
+	public void TakeDamage (int _damage) 
+	{
+		currentHealth -= _damage;
+
+
+		if (currentHealth <= 0) 
+		{
+			//Call Die Method
+			Die();
+		}
+
+	}
+	void Die()
+	{
+		//Kill the Enemy
+		//Play an animation
+		//Instantiate a ragdoll
+
+		//Destroy the Enemy GO
+		Debug.Log("GAME OVER");
+	}	
+}
